386bd513fbe589f72421ebda0fee6c2e39a98ab6
rajesh chaudhari <93049758+rajeshchau@users.noreply.github.com>
2025-05-11 13:57:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 function mincost(arr)
 { 
+	let current = arr
+	let final = []
+	let total = 0
+	let i = 0
+
+	function assending(){
+		current = current.sort((a,b)=>a-b);
+	}
+
+	while (current.length <=1) {
+		assending();
+		total = current[i]+current[i+1];
+		current.pop(current[i]);
+		current.pop(current[i+1]);
+		current.push(total);
+		final.push(total);
+		total=0;
+		if (current.length <=1) {
+			final.foreach((data)=>{
+				 return total +=data;
+			})
+		}
+	}
+
+	
 //write your code here
 // return the min cost
   
